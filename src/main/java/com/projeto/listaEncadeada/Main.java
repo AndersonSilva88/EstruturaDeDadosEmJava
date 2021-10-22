9e0b8223cd61e191e7df6f21cd84e58f5fa3568d
perkles <anderson.os88@hotmail.com>
2021-10-21 21:21:45 -0300

    listas final

--- src/main/java/com/projeto/listaEncadeada/Main.java
+++ src/main/java/com/projeto/listaEncadeada/Main.java
@@ -3,5 +3,16 @@ package com.projeto.listaEncadeada;
 public class Main {
     public static void main(String[] args) {
 
+        ListaEncadeada<String> minhaListaEncadeada = new ListaEncadeada<>();
+
+        minhaListaEncadeada.add("teste1");
+        minhaListaEncadeada.add("teste2");
+        minhaListaEncadeada.add("teste3");
+        minhaListaEncadeada.add("teste4");
+
+        System.out.println(minhaListaEncadeada);
+        System.out.println(minhaListaEncadeada.get(0));
+        System.out.println(minhaListaEncadeada.get(1));
+
     }
 }
