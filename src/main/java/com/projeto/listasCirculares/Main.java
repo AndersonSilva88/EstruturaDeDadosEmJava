e3ba791bf73f46316cd3b35345e1abe91fe906e5
perkles <anderson.os88@hotmail.com>
2021-10-23 20:26:36 -0300

    lista circular

---
+++ src/main/java/com/projeto/listasCirculares/Main.java
@@ -0,0 +1,23 @@
+package com.projeto.listasCirculares;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ListaCircular<String> minhaListaCircular = new ListaCircular<>();
+
+        minhaListaCircular.add("0");
+        System.out.println(minhaListaCircular);
+
+        minhaListaCircular.remove(0);
+
+        System.out.println(minhaListaCircular);
+
+        minhaListaCircular.add("1");
+        minhaListaCircular.add("2");
+        System.out.println(minhaListaCircular);
+
+        System.out.println(minhaListaCircular.get(0));
+        System.out.println(minhaListaCircular.get(2));
+        System.out.println(minhaListaCircular.get(3));
+    }
+}
